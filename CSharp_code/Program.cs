d8c0cc34c31864e16163d9e48075bf34dbf2d6f5
Akash Jatwa <55755027+akashjatwa@users.noreply.github.com>
2025-07-08 21:13:43 +0530

    Add C# console application

---
+++ CSharp_code/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
